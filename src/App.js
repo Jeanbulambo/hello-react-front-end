387d68854839e5b591d8dbf54772d618930e5f74
Jean Bulambo <jeanbulambo4@gmail.com>
2022-12-03 01:17:21 +0200

    Add linters config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter, Routes, Route, NavLink,
+} from 'react-router-dom';
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavLink route to='/'> Home</NavLink>
-      <NavLink route to='/greeting'>Greeting</NavLink>
+      <NavLink route to="/"> Greeting</NavLink>
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/greeting' element={<Greeting />} />
-      </Routes> 
+        <Route exact path="/" element={<Greeting />} />
+      </Routes>
     </BrowserRouter>
   );
 }
